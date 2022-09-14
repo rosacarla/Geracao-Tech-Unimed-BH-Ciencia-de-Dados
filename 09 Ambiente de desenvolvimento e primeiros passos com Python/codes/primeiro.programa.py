1d72797bd8d7f5bf75168c2a98da552ec0e8c3b9
Carla Edila Silveira <rosa.carla@pucpr.edu.br>
2022-09-13 21:18:44 -0300

    Upload first folders

---
+++ 09 Ambiente de desenvolvimento e primeiros passos com Python/codes/primeiro.programa.py
@@ -0,0 +1,8 @@
+# Programa: imprimir textos na tela
+# Autora: Carla Edila Silveira
+# Data: 13-09-2022
+
+print() # 1 linha em branco
+print("Hello, world!")
+print("\n") # 2 linhas em branco
+print("Welcome to Python course.")
